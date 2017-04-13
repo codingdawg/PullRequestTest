5234b658987149751b810ffb9d09712d5c97fc6b
Praveen Reddy <praveen.reddy@docusign.com>
2017-04-13 06:16:11 -0700

    Adding the Person class

---
+++ Person.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Hello.App
+{
+    public class Person
+    {
+        public string Name { get;  set; }
+
+        public int Age { get;  set; }
+
+        public string Gender { get;  set; }
+
+        public string DOB { get;  set; }
+
+        public string MaritalStatus { get;  set; }
+    }
+}
